5f8a948443849da2669fe4e6d4e6f2763fa09c6c
SLaks <Dev@SLaks.net>
2015-07-28 09:12:16 -0400

    Randomly choose an image set for Intro

--- NetPresenter/Views/IntroView.xaml.cs
+++ NetPresenter/Views/IntroView.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
@@ -29,24 +30,57 @@ namespace NetPresenter.Views {
 			Loaded += delegate { sparkleTimer.Start(); };
 		}
 
+		public IntroView(IReadOnlyDictionary<string, string> set)
+			: this(set["Background"], set["Foreground"]) {
+		}
+
 
 		///<summary>Creates an <see cref="IntroView"/> creator function from background & foreground images in the specified directory, or null if no such images exist.</summary>
 		public static Func<IntroView> TryCreateFactory(string directory) {
 			directory = IO.Path.GetFullPath(directory);
 			if (!Directory.Exists(directory))
 				return null;
-			var foreground = TryGetImage(directory, "Foreground");
-			var background = TryGetImage(directory, "Background");
-			if (foreground == null || background == null)
+			var sets = GetImageSets(directory, "Foreground", "Background");
+			if (!sets.Any())
 				return null;
-			return () => new IntroView(background, foreground);
+
+			Random rand = new Random();
+			return () => new IntroView(sets[rand.Next(sets.Count)]);
 		}
 
 		static ISet<string> imageExtensions = new HashSet<string>(StringComparer.OrdinalIgnoreCase) { ".png", ".jpg", ".jpeg" };
-		///<summary>Attempts to locate an image file with the specified name.</summary>
-		static string TryGetImage(string directory, string name) {
-			return Directory.EnumerateFiles(directory, IO.Path.ChangeExtension(name, null) + ".*")
-				.FirstOrDefault(p => imageExtensions.Contains(IO.Path.GetExtension(p)));
+		///<summary>Returns all complete image groups with the specified keys in a directory.</summary>
+		///<returns>A collection of maps of keys to full paths.</returns>
+		///<remarks>
+		/// An image group is a set of images with the same suffix with one prefix for each key.
+		/// For example, if the keys set contains "Background" and "Foreground", and the folder
+		/// holds "Background-Light", "Background-Dark", "Foreground-Light", "Foreground-Dark",
+		/// and "Foreground-Other", this method will return two groups:
+		/// ("Background-Light", "Foreground-Light") and ("Background-Dark", "Foreground-Dark").
+		///</remarks>
+		static IReadOnlyList<IReadOnlyDictionary<string, string>> GetImageSets(string directory, params string[] keys) {
+			var nameMatcher = new Regex(
+				@"^(?<key>" + string.Join("|", keys.Select(Regex.Escape)) + ")"
+			  + @"(?<suffix>.*)"
+			  + @"\.(?:" + string.Join("|", imageExtensions.Select(s => s.TrimStart('.'))) + @")$",
+				RegexOptions.IgnoreCase
+			);
+
+			return Directory.EnumerateFiles(directory)
+							.Select(IO.Path.GetFileName)
+							.Select(p => nameMatcher.Match(p))
+							.Where(m => m.Success)
+							.GroupBy(m => m.Groups["suffix"].Value)
+							.Where(g => !keys.Except(
+								g.Select(m => m.Groups["key"].Value),
+								StringComparer.OrdinalIgnoreCase
+							).Any())
+							.Select(g => g.ToDictionary(
+								m => m.Groups["key"].Value,
+								m => IO.Path.GetFullPath(IO.Path.Combine(directory, m.Value)),
+								StringComparer.OrdinalIgnoreCase
+							))
+							.ToList();
 		}
 
 		readonly Random rand = new Random();
